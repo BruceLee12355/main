b91467d63f2500809d5666cdc258efb22d6695ac
BruceLee315 <mrpropper315@gmail.com>
2023-04-24 17:11:17 +0300

    Revert "adding operation "int"" If you need it back This reverts commit 0cf322d6b57305c1e59804015278068462b46c1a.

--- main.cpp
+++ main.cpp
@@ -2,11 +2,10 @@
 
 int main()
 {
-int name_user1, name_user2;
 std::cout << "first user, please, Enter your name: ";
 std::cin >> name_user1;
 std::cout << "Hello " << name_user1 << endl;
-std::cout << "second user, please, Enter your name: ";
+std::cout << "second user, please, Enter your name: "
 std::cin >> name_user2;
 std::cout << "Hello " << name_user2 << endl;
 
